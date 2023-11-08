5c28701aa6688911f6dec5aec1a72c63f8dd467a
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-11-08 16:08:52 +0100

    Spawn random enemy blocks -04

--- project_4/script.js
+++ project_4/script.js
@@ -1,9 +1,42 @@
 const player = document.querySelector(".player");
+const game = document.querySelector("#game-area");
 let posX = 100;
 let posY = 100;
+let lives = 3;
+let startTime; 
 
-document.body.addEventListener("keyup", (event) => {
-    const game = document.querySelector("#game-area");
+
+initializeGame()
+
+function initializeGame() {
+    posX = 100;
+    posY = 100;
+    lives = 3;
+
+    startTime = Date.now();
+    console.log(startTime);
+    setInterval(() => {
+        const timerElement = document.querySelector(".timer");
+        timerElement.textContent = ((Date.now() - startTime) / 1000).toFixed(0);
+
+        updateEnemyPositions();
+    }, 100);
+
+    player.style.top = posY + "px";
+    player.style.left = posX + "px";
+    
+    for (let index = 1; index <= 4; index++) {
+        const enemy = document.createElement("div");
+        enemy.classList.add("enemy");
+
+        enemy.style.top = Math.round(Math.random() * 300 + 200) + "px";
+        enemy.style.left = Math.round(Math.random() * 700 + 200) + "px";
+
+        game.appendChild(enemy);
+    }
+}
+
+document.body.addEventListener("keydown", (event) => {
     switch (event.key) {
         case "ArrowUp":
             if (posY > 0)
@@ -27,7 +60,7 @@ document.body.addEventListener("keyup", (event) => {
             }
             break;
         case "ArrowRight":
-            if (posX < game.clientHeight - 50)
+            if (posX < game.clientWidth - 50)
             {
                 posX += 5;
                 player.style.left = posX + "px";
